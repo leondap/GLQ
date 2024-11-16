00b8d8f87643daccf221b17ab7a173d52616b90a
Leonardo Dapporto <68218234+leondap@users.noreply.github.com>
2024-11-16 16:40:00 +0100

    Update recluster.landscape.R

--- R/recluster.landscape.R
+++ R/recluster.landscape.R
@@ -1,4 +1,4 @@
-recluster.landscape<-function (mat, units = NULL, dist, transcorr=null, map_alt, map1=NULL, mapplot=NULL, minsea = 2, plot=F) 
+recluster.landscape<-function (mat, units = NULL, dist, transcorr=NULL, map_alt=NULL, map1=NULL, mapplot=NULL, minsea = 2, plot=T) 
 {
 	gendistances <- as.matrix(dist)
 	mat[, 1] <- mat[, 1] + runif(nrow(mat), min = 0.001, max = 0.002)
@@ -23,6 +23,13 @@ recluster.landscape<-function (mat, units = NULL, dist, transcorr=null, map_alt,
 			sp::plot(mapplot, add=T)
 		}
 	}
+	seapointx<-rep(NA,nrow(try$delsgs))
+	seapointy<-seapointx
+	lengthsea<-seapointx
+	altimin<-seapointx
+	altimax<-seapointx
+
+
 	for (i in 1:nrow(try$delsgs)) {
 #i<-2
 		gendist[i] <- gendistances[try$delsgs[i, 5], try$delsgs[i, 6]]
@@ -44,45 +51,53 @@ recluster.landscape<-function (mat, units = NULL, dist, transcorr=null, map_alt,
 		if(plot){
 			plot(pa, add=T)
 		}
-      	nume <- as.numeric(round(st_length(pa) * 0.0003, 0))
-		punti <- st_sample(pa, nume, type = "regular")
-		coords <- (st_coordinates(punti)[, 1:2])
-		coords <- rbind(c(10, 10), coords)
-		value <- c(as.vector(extract(map1, coords)))
-		altitudes <- as.data.frame(as.vector(extract(map_alt, coords)))
-		val <- cbind(coords, value, altitudes)
-		altimin[i] <- min(val[, 4], na.rm = T)
-		altimax[i] <- max(val[, 4], na.rm = T)
 		lengthpath[i] <- st_length(pa)*0.001
-		if (nume == 1) {
-			midpointx[i] <- (try$delsgs[i, 1] + try$delsgs[i, 3])/2
-            	midpointy[i] <- (try$delsgs[i, 2] + try$delsgs[i, 4])/2
-            	seapointx[i] <- (try$delsgs[i, 1] + try$delsgs[i, 3])/2
-            	seapointy[i] <- (try$delsgs[i, 2] + try$delsgs[i, 4])/2
-            	lengthsea[i] <- 0
-		}
-		if (nume > 1) {
-			val <- val[-1, ]
-			tabe <- with(rle(val[, 3]), {
-			ok <- values == 1
-			ends <- cumsum(lengths)[ok]
-			starts <- ends - lengths[ok] + 1
-			resi <- as.data.frame(cbind(starts, ends))
-			return(resi)
-		})
-		tabe <- cbind(tabe, (tabe[, 2] - tabe[, 1] + 1))
-		tabe <- tabe[which(tabe[, 3] >= minsea), ]
-		line <- which.max(tabe[, 3])[1]
-		midpointx[i] <- as.numeric(val[round(nrow(val)/2, 0), 1])
-		midpointy[i] <- as.numeric(val[round(nrow(val)/2, 0), 2])
-		lengthsea[i] <- sum(tabe[, 3])/3
-		if (lengthsea[i] > 0) {
-            	seapointx[i] <- as.numeric(val[round((tabe[line, 1] + tabe[line, 2])/2, 0), 1])
-			seapointy[i] <- as.numeric(val[round((tabe[line, 1] + tabe[line, 2])/2, 0), 2])
-		}
-		if (lengthsea[i] == 0) {
-			seapointx[i] <- midpointx[i]
-			seapointy[i] <- midpointy[i]
+		midpointx[i] <- (try$delsgs[i, 1]+try$delsgs[i, 3])/2
+		midpointy[i] <-  (try$delsgs[i, 2]+try$delsgs[i, 4])/2
+     	
+			if(!(is.null(map1))){
+			nume <- as.numeric(round(st_length(pa) * 0.0003, 0))
+			punti <- st_sample(pa, nume, type = "regular")
+			coords <- (st_coordinates(punti)[, 1:2])
+			coords <- rbind(c(10, 10), coords)
+			value <- c(as.vector(extract(map1, coords)))
+			if(!(is.null(map_alt))){
+				altitudes <- as.data.frame(as.vector(extract(map_alt, coords)))
+				val <- cbind(coords, value, altitudes)
+				altimin[i] <- min(val[, 4], na.rm = T)
+				altimax[i] <- max(val[, 4], na.rm = T)
+			}else{
+				altimin[i] <- NA
+				altimax[i] <- NA
+			}
+		
+			if (nume == 1) {
+				seapointx[i] <- (try$delsgs[i, 1] + try$delsgs[i, 3])/2
+            		seapointy[i] <- (try$delsgs[i, 2] + try$delsgs[i, 4])/2
+            		lengthsea[i] <- 0
+			}
+			if (nume > 1) {
+				val <- val[-1, ]
+				tabe <- with(rle(val[, 3]), {
+				ok <- values == 1
+				ends <- cumsum(lengths)[ok]
+				starts <- ends - lengths[ok] + 1
+				resi <- as.data.frame(cbind(starts, ends))
+				return(resi)
+				})
+				tabe <- cbind(tabe, (tabe[, 2] - tabe[, 1] + 1))
+				tabe <- tabe[which(tabe[, 3] >= minsea), ]
+				line <- which.max(tabe[, 3])[1]
+		
+				lengthsea[i] <- sum(tabe[, 3])/3
+				if (lengthsea[i] > 0) {
+            			seapointx[i] <- as.numeric(val[round((tabe[line, 1] + tabe[line, 2])/2, 0), 1])
+					seapointy[i] <- as.numeric(val[round((tabe[line, 1] + tabe[line, 2])/2, 0), 2])
+				}
+				if (lengthsea[i] == 0) {
+					seapointx[i] <- midpointx[i]
+					seapointy[i] <- midpointy[i]
+				}
 			}
 		}
 	}
